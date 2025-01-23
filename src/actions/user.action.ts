2760e193f0fae474a29c8016808beed2dd6882ca
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2025-01-23 19:33:30 +0530

    Created Sidebar component for authenticated users and Unauthenticated users

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -37,3 +37,20 @@ export async function syncUser() {
     console.error("Error syncing user:", error);
   }
 }
+
+export async function getUserByClerkId(clerkId: string) {
+  return await prisma.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          following: true,
+          posts: true,
+        }
+      }
+    }
+  });
+}
