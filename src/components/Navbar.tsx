f1cc291f45cecb76f25269815549731955add2a0
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2025-01-23 17:51:16 +0530

    Server Action to sync the users of clerk with the prisma database

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,23 @@
-import Link from 'next/link'
-import React from 'react'
-import DesktopNavbar from './DesktopNavbar'
-import MobileNavbar from './MobileNavbar'
+import Link from "next/link";
+import React from "react";
+import DesktopNavbar from "./DesktopNavbar";
+import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await currentUser();
+  if (user) await syncUser(); // POST
+
   return (
-    <nav className='sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50'>
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
+            <Link
+              href="/"
+              className="text-xl font-bold text-primary font-mono tracking-wider"
+            >
               ImageVibe
             </Link>
           </div>
@@ -19,7 +27,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
