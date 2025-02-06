f3ac89c42dfb1d1346cd6d01009e0c38450ac2d2
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2025-02-06 23:52:58 +0530

    Implemented the Image upload Functionality

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -9,6 +9,7 @@ import { Button } from "./ui/button";
 import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { createPost } from "@/actions/post.action";
 import toast from "react-hot-toast";
+import ImageUpload from "./ImageUpload";
 
 function CreatePost() {
   const { user } = useUser();
@@ -60,7 +61,7 @@ function CreatePost() {
           </div>
 
           {/* Handle Image Upload */}
-          {/* {(showImageUpload || imageUrl) && (
+          {(showImageUpload || imageUrl) && (
             <div className="border rounded-lg p-4">
               <ImageUpload
                 endpoint="postImage"
@@ -71,7 +72,7 @@ function CreatePost() {
                 }}
               />
             </div>
-          )} */}
+          )}
 
           <div className="flex items-center justify-between border-t pt-4">
             <div className="flex space-x-2">
